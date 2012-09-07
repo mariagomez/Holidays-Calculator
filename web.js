96b3e2a125d2497b85497c94a11d79aedfd856d4
Maria <mgomez@thoughtworks.com>
2012-09-07 15:15:16 +0100

    Built form page

--- web.js
+++ web.js
@@ -2,11 +2,16 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.sendfile(__dirname + '/views/form.html');
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+
+
+
